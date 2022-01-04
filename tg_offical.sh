f5d98b09acb2f5fae4cad47970568c9c33705d6e
ChellyL <38887991+ChellyL@users.noreply.github.com>
2022-01-04 23:51:32 +0800

    Create tg_offical.sh

---
+++ tg_offical.sh
@@ -0,0 +1,60 @@
+echo ""
+echo "######################################"
+echo "   Telegram Messenger MTProto 安装    "
+echo "######################################"
+echo ""
+echo "* 使用 telegrammessenger 官方 docker 镜像 "
+echo "https://hub.docker.com/r/telegrammessenger/proxy/"
+echo ""
+echo " 安装时可能需要手动确认安装（回车即可）"
+echo ""
+
+echo "前序准备ing"
+
+apt update
+apt install docker
+apt install docker-compose
+
+ip=$(curl -4 ip.sb)
+hex=$(openssl rand -hex 16)
+
+echo ""
+echo "准备完毕～"
+echo ""
+
+read -p "请设置端口（1-65535）（默认 443）：" PORT
+if [[ -n $PORT ]];then
+  port=$PORT
+else
+  port=443
+fi
+echo "端口为 $port"
+
+read -p "请设置secret（回车自动生成）：" SECRET
+if [[ -n $SECRET ]];then
+  secret=$SECRET
+else
+  secret=$hex
+fi
+echo " secret为 $secret"
+
+read -p "是否添加广告频道[y/n]：" AD
+if [[ $AD=~ y|Y ]];then
+	read -p "请输入频道TAG：" tag
+  docker pull telegrammessenger/proxy
+  docker run -d -p$port:443 --name=mtproto-proxy --restart=always -v proxy-config:/data -e SECRET=$secret TAG=$tag telegrammessenger/proxy:latest
+
+else
+	docker pull telegrammessenger/proxy
+  docker run -d -p$port:443 --name=mtproto-proxy --restart=always -v proxy-config:/data -e SECRET=$secret telegrammessenger/proxy:latest
+
+fi
+
+echo "链接为 tg://proxy?server=$ip&port=$port&secret=$secret"
+echo ""
+echo "配置文件位于 mtp.txt 中"
+cat > ./mtp.txt << EOF
+tg://proxy?server=$ip&port=$port&secret=$secret
+EOF
+
+
